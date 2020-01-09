d39e46eb676e478d269169a7de600faaa305db8b
Dawid Staszak <Dawid.Staszak.95@gmail.com>
2020-01-10 00:02:31 +0100

    Listener
    Dodano klasę MyTcpListener

---
+++ MyTCPListener.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ProjektKlient_Server
+{
+    class MyTcpListener
+    {
+        TcpListener server = null;
+        TcpClient client = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="port">Podaj port z którego ma korzystać serwer</param>
+        public void CreateAndStartListeningServer(Int32 port)
+        {
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+        }
+        public bool WaitForClient() {
+            try
+            {
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Wait for Client
+                    client = server.AcceptTcpClient();
+                    return true;
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                server.Stop();
+                client = null;
+                server = null;
+            }
+            return false;
+        }
+        public void ReadData()
+        {
+            try
+            {
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    // Received
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    // Processing
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    // Response
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                server.Stop();
+            }
+        }
+    }
+}
+
